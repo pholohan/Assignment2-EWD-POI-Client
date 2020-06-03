3dd9e1062cc1e3de8fc13e9afc631fcd95cbb740
pholohan <holohan.paul@gmail.com>
2020-06-03 17:50:09 +0100

    HTTP Client Configured and User Models incorporated with Login & Signup

--- src/services/stadium-service.ts
+++ src/services/stadium-service.ts
@@ -1,5 +1,58 @@
-import { Stadium } from './Stadium-types';
+import { inject, Aurelia } from 'aurelia-framework';
+import { Router } from 'aurelia-router';
+import { PLATFORM } from 'aurelia-pal';
+import { Stadium, User } from './Stadium-types';
+import { HttpClient } from 'aurelia-http-client';
 
+@inject(HttpClient, Aurelia, Router)
 export class StadiumService {
   stadiums: Stadium[] = [];
+  users: Map<string, User> = new Map();
+
+  constructor(private httpClient: HttpClient, private au: Aurelia, private router: Router) {
+    httpClient.configure(http => {
+      http.withBaseUrl('http://localhost:8080');
+    });
+    this.getStadiums();
+    this.getUsers();
+  }
+
+  async getStadiums() {
+    const response = await this.httpClient.get('/api/stadiums.json');
+    this.stadiums = await response.content;
+    console.log(this.stadiums);
+  }
+
+  async getUsers() {
+    const response = await this.httpClient.get('/api/users.json');
+    const users = await response.content;
+    users.forEach(user => {
+      this.users.set(user.email, user);
+    });
+  }
+
+  signup(firstName: string, lastName: string, email: string, password: string) {
+    //this.changeRouter(PLATFORM.moduleName('app'))
+    return false;
+  }
+
+  async login(email: string, password: string) {
+    const user = this.users.get(email);
+    if (user && (user.password === password)) {
+      this.changeRouter(PLATFORM.moduleName('app'))
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  logout() {
+    this.changeRouter(PLATFORM.moduleName('start'))
+  }
+
+  changeRouter(module:string) {
+    this.router.navigate('/', { replace: true, trigger: false });
+    this.router.reset();
+    this.au.setRoot(PLATFORM.moduleName(module));
+  }
 }
